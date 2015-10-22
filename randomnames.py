225c956283da1435d84f177d2fee0e37c37fa1df
Terry Bolt <GoldenBadger@users.noreply.github.com>
2015-10-22 16:25:03 +0100

    Update randomnames.py

--- randomnames.py
+++ randomnames.py
@@ -9,7 +9,7 @@ def generate_random_names(count):
         first_names.append(split_names[0])
         last_names.append(split_names[1])
     output_names = []
-    for i in range(count):
+    for i in range(count - 1):
         first_name = random.choice(first_names)
         last_name = random.choice(last_names)
         output_names.append(first_name + " " + last_name)
